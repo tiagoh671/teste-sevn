021c257dc0d76dbc4a241ec996cc6f52af64c4f8
tiagoh671 <tiagoh066.ts@gmail.com>
2024-09-17 19:01:25 -0300

    Renderização de alguns elementos na tela, juntamente com as devidas alterações no html e css para estilização

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+async function criarTabelas(rodada){
+  const tabela = document.createElement('tr')
+  tabela.id = tabela.round
 
+  tabela.innerHTML = `
+    <td id=${rodada.team_home_id}>${rodada.team_home_name}</td>
+    <td>${rodada.team_home_score}</td>
+    <td>X</td>
+    <td>${rodada.team_away_score}</td>
+    <td id=${rodada.team_away_id}>${rodada.team_away_name}</td>
+  `
+  document.querySelector('#tabela').append(tabela)
+}
 
 async function getRodadas() {
   const response = await fetch('https://sevn-pleno-esportes.deno.dev/')
   const rodadas = await response.json()
 
-  console.log(rodadas)
+ 
+  rodadas[0].games.forEach(criarTabelas)
 }
 
 getRodadas()
